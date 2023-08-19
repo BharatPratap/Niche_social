8279340c1ba081e53706ee99fe81cb74e97d74f0
BPratap <bp171020@gmail.com>
2023-08-19 16:20:22 +0530

    feat: :sparkles: added login and register routes

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const authRoutes = require("./routes/auth");
+
+dotenv.config();
+
+const app = express();
+
+// Demonstrate the readyState and on event emitters
+console.log(mongoose.connection.readyState); //logs 0
+mongoose.connection.on('connecting', () => {
+    console.log('connecting')
+    console.log(mongoose.connection.readyState); //logs 2
+});
+mongoose.connection.on('connected', () => {
+    console.log('connected');
+    console.log(mongoose.connection.readyState); //logs 1
+});
+mongoose.connection.on('disconnecting', () => {
+    console.log('disconnecting');
+    console.log(mongoose.connection.readyState); // logs 3
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('disconnected');
+    console.log(mongoose.connection.readyState); //logs 0
+});
+// Connect to a MongoDB server running on 'localhost:27017' and use the
+// 'test' database.
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true // Boilerplate for Mongoose 5.x
+});
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to homepage");
+});
+
+app.get("/users", (req, res) => {
+    res.send("Welcome to users");
+});
+
+
+
+app.listen(8800, () => {
+    console.log("Backend server is running!");
+    console.log("On port 8800");
+});
---
+++ routes/auth.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+const getHash = async () => {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    return salt;
+}
+//REGISTER
+router.post("/register", async (req, res) => {
+    try {
+        const userDetails = req.body;
+
+        // generate hashedPassword for user
+        const salt = await getHash();
+        const hashedPassword = await bcrypt.hash(userDetails.password, salt);
+
+        // create new user with hashed password
+        const newUser = new User({
+            username: userDetails.username,
+            email: userDetails.email,
+            password: hashedPassword,
+        })
+
+        // save the user in DB
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (error) {
+        console.log(`Error occured while creating the object-, ${error}`);
+        res.status(500).send(`Error occured while creating the object-, ${error}`);
+    }
+
+})
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username || '' });
+
+        !user && res.status(404).send("user not found");
+
+        const validUser = await bcrypt.compare(req.body.password, user.password);
+
+        !validUser && res.status(400).send(`Unauthorised- Invalid Password`);
+
+        res.status(200).send(`Welcome ${user.username} \n ${user}`);
+    } catch (error) {
+        console.log(`Error occured in login route-, ${error}`);
+        // res.status(500).json(error);
+    }
+});
+
+module.exports = router
